e361c6c5a8a5026091a61275dc81a07b7052c285
mPeskov23 <sorrowdelivery@gmail.com>
2024-06-20 18:29:35 +0200

    Added cors dependency

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // app.js
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
